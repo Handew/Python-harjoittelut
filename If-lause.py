cd5b3f98162f240bbdf696b02e0d8c6c96551edf
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-10 21:15:14 +0300

    Lisätty else if rakenne

--- If-lause.py
+++ If-lause.py
@@ -2,5 +2,10 @@ luku = 80
 
 if luku > 100:
     print("Luku on yli 100!")
-    print("If-lause loppuu")
+elif luku > 50:
+    print("Luku on yli 50!")
+else:
+    print("Luku on alle 50!")
+    
+print("If-lause loppuu")
 print("Sovellus loppuu!")
