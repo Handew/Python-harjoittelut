a4c832112a409472399625cb01d962d30c8d3e54
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-16 18:26:14 +0300

    Luokka / funktio harjoitteluita

---
+++ Funktiot.py
@@ -0,0 +1,9 @@
+print("Suoritus alkaa...")
+
+def oma_funktio():
+    print("Ollaan omassa funktiossa...")
+
+oma_funktio()
+
+
+print("Suoritus päättyy...")
--- Luokka.py
+++ Luokka.py
@@ -1,13 +1,26 @@
 class OmaLuokka:
     oma_arvo = 100
 
+    def testi(self):
+        print("OmaLuokka.testi -metodissa...")
+
+class Auto:
+    huippunopeus = 0
+
+# Periminen tapahtuu suluilla
+class UrheiluAuto(Auto):
+    pass
+
+
 
 print("Suoritus alkaa")
 
 instanssi = OmaLuokka()
+instanssi.testi()
+
 print(instanssi.oma_arvo)
 instanssi.oma_arvo = 200
 print(instanssi.oma_arvo)
 
 
-print("Instanssi luotu")
+print("Suoritus päättyy")
