4277173076ffda1b8b677eede58d972f429afe38
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-16 18:01:12 +0300

    Olion perusteet ja periytyminen

---
+++ Luokka.py
@@ -0,0 +1,13 @@
+class OmaLuokka:
+    oma_arvo = 100
+
+
+print("Suoritus alkaa")
+
+instanssi = OmaLuokka()
+print(instanssi.oma_arvo)
+instanssi.oma_arvo = 200
+print(instanssi.oma_arvo)
+
+
+print("Instanssi luotu")
