814341f04dffa609e1e1746fa567554577ba1eac
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-17 21:37:54 +0300

    Tiedoston luku ja summan laskenta

---
+++ Tiedostosumma.py
@@ -0,0 +1,19 @@
+print("Aloitetaan tiedoston käsittely.")
+
+tiedosto = open("Luvut.txt", "r")
+#data = tiedosto.read()
+#print(data)
+
+lukujen_summa = 0
+rivi_nro = 1
+for rivi in tiedosto:
+    print(str(rivi_nro) + ": " + rivi)
+    rivi_nro += 1
+    lukujen_summa += int(rivi)
+
+tiedosto.close()
+
+print("Tiedostosta luettujen lukujen summa: ")
+print(lukujen_summa)
+print()
+print("Suoritus päättyy.")
