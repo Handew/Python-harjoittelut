7540955675da77b4925c67464dddb0da3fc5a74a
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-18 13:18:31 +0300

    Tyyppi testailuja ja perimistä

---
+++ Auto.py
@@ -0,0 +1,7 @@
+class Auto:
+    def kaynnista(self):
+        print("Käynnistetään auton moottori.")
+
+
+print("Ollaan Auto.py-tiedostossa!")
+print(__name__)
---
+++ Tyypit.py
@@ -0,0 +1,11 @@
+import Auto
+
+kokonaisluku = 123
+liukuluku = 1.234
+merkkijono = "ABCD"
+auto = Auto.Auto()
+
+print(type(kokonaisluku))
+print(type(liukuluku))
+print(type(merkkijono))
+print(type(auto))
