8219e736ed270839d22a8dee94b7b535bf9b9cd6
Hannu Ilvonen <hannu.ilvonen@student.careeria.fi>
2020-08-18 15:49:56 +0300

    Sekvenssit

---
+++ Sekvenssit.py
@@ -0,0 +1,12 @@
+lista = []
+
+nimet = ["Jaska", "Kalle", "Maija","Keke", "Kekkonen", "Reiska"]
+
+lista.append(123)
+lista.append(234)
+lista.append("ABC")
+
+print(lista)
+
+lkm = nimet.count("Jaska")
+print(f"Jaska: {lkm}")
